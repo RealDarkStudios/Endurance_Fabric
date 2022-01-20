74999ad98db40a88015553473219cb9abfb14112
realDarkGamerYT <jmorton@digitalunderworlds.com>
2022-01-20 08:53:28 -0600

    Version 1.17.1-0.2.3 (08:53 CST Jan 20, 2022): Add Redwood Door and Trapdoor.

--- src/main/java/net/thedarkgamer/endurance/block/ModBlocks.java
+++ src/main/java/net/thedarkgamer/endurance/block/ModBlocks.java
@@ -5,19 +5,14 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 import net.thedarkgamer.endurance.EnduranceMod;
 import net.thedarkgamer.endurance.block.custom.MedicalUpgraderBlock;
-import net.thedarkgamer.endurance.block.custom.blocktypes.ModPressurePlateBlock;
-import net.thedarkgamer.endurance.block.custom.blocktypes.ModSaplingBlock;
-import net.thedarkgamer.endurance.block.custom.blocktypes.ModStairsBlock;
-import net.thedarkgamer.endurance.block.custom.blocktypes.ModWoodenButtonBlock;
+import net.thedarkgamer.endurance.block.custom.blocktypes.*;
 import net.thedarkgamer.endurance.item.ModItemGroups;
 import net.thedarkgamer.endurance.world.features.tree.RedwoodSaplingGenerator;
-import org.lwjgl.system.CallbackI;
 
 public class ModBlocks {
 
@@ -35,6 +30,8 @@ public class ModBlocks {
     public static final Block REDWOOD_FENCE_GATE = registerBlock("redwood_fence_gate", new FenceGateBlock(FabricBlockSettings.copy(Blocks.OAK_FENCE_GATE)), new FabricItemSettings().rarity(Rarity.COMMON).group(ModItemGroups.BLOCKS));
     public static final Block REDWOOD_PRESSURE_PLATE = registerBlock("redwood_pressure_plate", new ModPressurePlateBlock(PressurePlateBlock.ActivationRule.EVERYTHING, FabricBlockSettings.copy(Blocks.OAK_PRESSURE_PLATE)), new FabricItemSettings().rarity(Rarity.COMMON).group(ModItemGroups.BLOCKS));
     public static final Block REDWOOD_BUTTON = registerBlock("redwood_button", new ModWoodenButtonBlock(FabricBlockSettings.copy(Blocks.OAK_BUTTON)), new FabricItemSettings().rarity(Rarity.COMMON).group(ModItemGroups.BLOCKS));
+    public static final Block REDWOOD_DOOR = registerBlock("redwood_door", new ModDoorBlock(FabricBlockSettings.copy(Blocks.OAK_DOOR)), new FabricItemSettings().group(ModItemGroups.BLOCKS).rarity(Rarity.COMMON));
+    public static final Block REDWOOD_TRAPDOOR = registerBlock("redwood_trapdoor", new ModTrapdoorBlock(FabricBlockSettings.copy(Blocks.OAK_TRAPDOOR)), new FabricItemSettings().group(ModItemGroups.BLOCKS).rarity(Rarity.COMMON));
 
     //Ores
     public static final Block TIN_ORE = registerBlock("tin_ore", new Block(FabricBlockSettings.copy(Blocks.IRON_ORE)), new FabricItemSettings().rarity(Rarity.COMMON).group(ModItemGroups.ORES_AND_INGOTS));
@@ -43,7 +40,7 @@ public class ModBlocks {
     public static final Block MEDICAL_UPGRADER = registerBlock("medical_upgrader", new MedicalUpgraderBlock(FabricBlockSettings.copy(Blocks.IRON_BLOCK)), new FabricItemSettings().rarity(Rarity.COMMON).group(ModItemGroups.BLOCKS));
 
     //Secrets - Only obtainable through commands, also hi to people who read code/comments :)
-    public static final Block NETHER_REACTOR_CORE = registerBlock("reactor_core", new Block(FabricBlockSettings.copy(Blocks.IRON_BLOCK)), new FabricItemSettings().rarity(Rarity.EPIC).group(ModItemGroups.BLOCKS));
+    public static final Block NETHER_REACTOR_CORE = registerBlock("reactor_core", new Block(FabricBlockSettings.copy(Blocks.IRON_BLOCK)), new FabricItemSettings().rarity(Rarity.EPIC));
 
     private static Block registerBlock(String name, Block block, FabricItemSettings settings) {
         registerBlockItem(name, block, settings);
---
+++ src/main/java/net/thedarkgamer/endurance/block/custom/blocktypes/ModTrapdoorBlock.java
@@ -0,0 +1,9 @@
+package net.thedarkgamer.endurance.block.custom.blocktypes;
+
+import net.minecraft.block.TrapdoorBlock;
+
+public class ModTrapdoorBlock extends TrapdoorBlock {
+    public ModTrapdoorBlock(Settings settings) {
+        super(settings);
+    }
+}
